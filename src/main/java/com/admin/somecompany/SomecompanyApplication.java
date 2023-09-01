5f28c6bfc5b3f5a22b7b18cff025fac540df5a29
Dyonya <dyonya@tut.by>
2023-09-01 17:30:26 +0300

    add spring initializer - Spring Web, Tymeleaf, Spring Boot DevTools

---
+++ src/main/java/com/admin/somecompany/SomecompanyApplication.java
@@ -0,0 +1,13 @@
+package com.admin.somecompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomecompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomecompanyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/admin/somecompany/SomecompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.admin.somecompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomecompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
